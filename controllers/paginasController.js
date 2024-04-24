d956b744a1a724c2a786ae873e6214fa5b695430
alejo de la arena <alejodelaarenaa@gmail.com>
2024-04-24 15:39:39 -0300

    Primer commit del proyecto Agencia de viajes

---
+++ controllers/paginasController.js
@@ -0,0 +1,60 @@
+import e from 'express';
+import { Viaje } from '../models/Viaje.js'
+
+const paginaInicio = (req, res) => { // req - lo que enviamos : res - lo que express nos responde
+    res.render('inicio', {
+        pagina: 'Inicio'
+    });
+};
+
+const paginaNosotros = (req, res) => {
+    res.render('nosotros', {
+        pagina: 'Nosotros'
+    });
+};
+
+const paginaViajes = async (req, res) => {
+    // Consultar BD
+    const viajes = await Viaje.findAll();
+    console.log(viajes)
+    
+
+    res.render('viajes', {
+        pagina: 'Próximos Viajes',
+        viajes,
+    });
+};
+
+const paginaTestimoniales = (req, res) => {
+    res.render('testimoniales', {
+        pagina: 'Testimoniales'
+    });
+};
+
+//Muestra un viaje por su slug
+const paginaDetalleViaje = async (req, res) => {
+
+    const { slug } = req.params;
+
+    
+    try {
+        const viaje = await Viaje.findOne( { where: { slug } } ) // Va a hacerle un where al que tenga el slug igual al del viaje correspondiente y se lo asignara al resultado 
+
+        res.render('viaje', {
+            pagina: 'Información Viaje', 
+            viaje
+        });
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}   
+
+export {
+    paginaInicio,
+    paginaNosotros,
+    paginaViajes,
+    paginaTestimoniales,
+    paginaDetalleViaje
+}
---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import router from './routes/index.js';
+import db from './config/db.js';
+
+const app = express();
+
+// Conectar a la db
+db.authenticate()
+    .then( () => console.log('BD conectada ')) 
+    .catch( error => console.log(error))
+
+const port = process.env.PORT || 4000;
+
+// Habilitar PUG 
+app.set('view engine', 'pug');
+
+// Obtener el año actual
+app.use( (req, res, next) => {
+
+    const year = new Date();
+    res.locals.actualYear = year.getFullYear();
+    res.locals.nombreSitio = "Agencia de Viajes";
+    next();
+})
+
+// Agregar body parser para leer los datos del formulario
+app.use(express.urlencoded({extended: true}));
+
+// Definir la carpeta public
+app.use(express.static('public'));
+
+// Agregar router
+app.use('/', router);
+
+app.listen(port, () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
+})
